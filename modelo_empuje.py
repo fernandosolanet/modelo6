a72acd212d8ff7e0a51c9197c3c03ebb1324f99f
fernandosolanet <36764400+fernandosolanet@users.noreply.github.com>
2018-03-07 12:07:35 +0100

    docstring e imports
    Cambio el docstring del módulo: el autor y la descripción. Borro la fecha.
    Sólo se importan las funciones que se usan.

--- modelo_empuje.py
+++ modelo_empuje.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Mar  5 13:13:16 2018
 
-@author: pedro.munoz
+@author: Team REOS
+
+Funciones de las curvas de empuje de los motores del F-18. Modelo GE F404.
+Previamente a este código, se han obtenido unas funciones de coeficientes
+relativas a las curvas de empuje de la aeronave.  Partiendo del empuje máximo
+del F-18 (el empuje máximo a nivel del mar) y con el dato de la altura, el cual
+nos dará el valor de densidad, podremos obtener el valor del empuje equivalente
+a cada altura en la que nos estemos moviendo.
+
 """
 
-from math import exp, radians, cos, pi, sin, degrees, atan, log10
-from modeloISA import density, temperature, GAMMA, viscosity, pressure, R_AIR
+from math import exp
+from modeloISA import density
 
-#---------------------------CURVAS DE EMPUJE DEL F-18---------------------------
-#Funciones de las curvas de empuje de los motores del F-18. Modelo GE F404.
-#Previamente a este código, se han obtenido unas funciones de coeficientes
-#relativas a las curvas de empuje de la aeronave.  Partiendo del empuje máximo
-#del F-18 (el empuje máximo a nivel del mar) y con el dato de la altura, el cual
-#nos dará el valor de densidad, podremos obtener el valor del empuje equivalente
-#a cada altura en la que nos estemos moviendo.
 
 TH_SL = 100000  # Empuje a nivel del mar (máximo).
 
@@ -24,7 +24,6 @@ def thrust(mach, den):
     Performance Calculations" en M. Saarlas, "Aircraft Performance", p. 273.
     Se tiene en cuenta que la aeronave cuenta con dos motores GE F404-400.
     '''
-    
     d_th = den / density(0)
     i = (.050618013228 + .11323534299 * d_th + 7.8263530571 * d_th**2
          - 15.012158645 * d_th**3)
