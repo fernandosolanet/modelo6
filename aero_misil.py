4fad2941e11510afb6464c06c303de026c1b47b9
PedroMMB <36886310+PedroMMB@users.noreply.github.com>
2018-03-16 10:58:53 +0100

    Update aero_misil.py
    Cambio del comentario

--- aero_misil.py
+++ aero_misil.py
@@ -3,7 +3,7 @@
 
 @author: Team REOS
 
-Funciones que nos dan los coeficientes aerodinámicos del avión F4.
+Funciones que nos dan los coeficientes aerodinámicos del cohete REOS.
 Coeficientes aerodinámicos de sustentación y de resistencia y todo aquello
 relacionado con los mismos.
 
