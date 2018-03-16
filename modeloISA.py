e0c58c4eb7194ec8d5f18ea7275ddb40a97e7d39
fernandosolanet <36764400+fernandosolanet@users.noreply.github.com>
2018-03-16 11:30:54 +0100

    Estilo y gravedad
    -Quito la gravedad del modelo ISA.
    -Aplico normas de estilo

--- modeloISA.py
+++ modeloISA.py
@@ -5,27 +5,24 @@
 
 Modelo atmosférico: ATMÓSFERA ESTÁNDAR INTERNACIONAL (ISA).
 
-Funciones de gradiente de temperatura (alf_isa), temperatura (temperature),
-densidad (density), presión (pressure) y viscosidad (viscosity).  Sólo
-requieren una variable de entrada: la altitud.
+Funciones de gradiente de temperatura (alf_isa), temperatura
+(temperature), densidad (density), presión (pressure) y viscosidad
+(viscosity).  Sólo requieren una variable de entrada: la altitud.
 
 """
 
 from math import exp
+from gravedad import MU, RT
 
-#Constantes atmosféricas.
+# Constantes atmosféricas.
 R_AIR = 287  # Constante de los gases ideales (J/Kkg)
-G = 6.673e-11  # Constante de gravitación universal (N m2/kg2).
-MT = 5.972e24  # Masa terrestre (kg).
-MU = G * MT
-RT = 6378136.3  # Radio terrestre (m).
 GRAV = MU / RT**2  # Aceleración de la gravedad a nivel del mar (m/s2)..
 RHO_SL = 101325 / (R_AIR * 288.15)  # Densidad a nivel del mar (kg/m3).
 GAMMA = 1.4  # Coeficiente de dilatación adiabática.
 BETA_VISC = .000001458  # Viscosidad de referencia (Pa s/K.5).
 S_VISC = 110.4  # Temperatura de referencia para la viscosidad (K).
 
-#Estas son las alturas estipuladas según la normativa.
+# Estas son las alturas estipuladas según la normativa.
 
 H_ISA1 = 11000
 H_ISA2 = 20000
@@ -35,12 +32,15 @@ H_ISA5 = 51000
 H_ISA6 = 71000
 H_ISA7 = 84852
 
-#Ahora se programan las variables termodinámicas, en función de la altura,
+# Ahora se programan las variables termodinámicas, en función de la altura,
 # y se relacionarán con los valores de T y alfa para cada altura estipulada.
+
 
 def alfa_isa(alt):
     '''Parámetro alfa de la ISA.  Este parámetro proporciona el gradiente de
     temperatura en K/m.
+
+    alt: altitud (m)
     '''
     if alt < H_ISA1:
         alf = -.0065
@@ -61,8 +61,10 @@ def alfa_isa(alt):
     return alf
 
 def temperature(alt):
-    '''Cálculo de la temperatura en función de la altura dada por el modelo
-    ISA.
+    '''Cálculo de la temperatura en función de la altura dada por el
+    modelo ISA.
+
+    alt: altitud (m)
     '''
     if alt < H_ISA1:
         h_0 = 0
@@ -90,15 +92,20 @@ def temperature(alt):
         t_0 = 214.65 - .002 * (H_ISA7 - H_ISA6)
     return t_0 + alfa_isa(alt) * (alt - h_0)
 
+
 def density(alt):
-    '''Cálculo de la densidad en función de la altura dada por el modelo ISA.
-    Se implementa el principio de Pascal.
+    '''Cálculo de la densidad en función de la altura dada por el modelo
+    ISA en kg/m3.  Se implementa el principio de Pascal.
+
+    alt: altitud (m)
     '''
     t_isa = temperature(alt)
+
     def density_alfa_0(altit, rho_0, alt_0):
         '''Fórmula de la densidad cuando alfa es 0.
         '''
         return rho_0 * exp(-GRAV * (altit - alt_0) / (R_AIR * t_isa))
+
     def density_alfa_no0(rho_0, temp_0, temp_m, alfa_den):
         '''Fórmula de la densidad cuando alfa no es 0.
         '''
@@ -152,22 +159,21 @@ def density(alt):
         return density_alfa_0(alt, rho0, h_0)
     return density_alfa_no0(rho0, t_0, t_isa, alf)
 
+
 def pressure(alt):
-    '''Cálculo de la presión en función de la altura dada por el modelo ISA.
-    Se implementa la ley de los gases ideales.
+    '''Cálculo de la presión en función de la altura dada por el modelo
+    ISA en Pa.  Se implementa la ley de los gases ideales.
+
+    alt: altitud (m)
     '''
     return density(alt) * R_AIR * temperature(alt)
 
+
 def viscosity(alt):
-    '''Cálculo de la viscosidad en función de la altura dada por el modelo ISA.
-    Se implementa la ley de Sutherland.
+    '''Cálculo de la viscosidad en función de la altura dada por el
+    modelo ISA en Pa s.  Se implementa la ley de Sutherland.
+
+    alt: altitud (m)
     '''
     temp = temperature(alt)
     return BETA_VISC * temp**(3 / 2) / (temp + S_VISC)
-
-def gravity(alt):
-    '''Cálculo de la gravedad en función de la altura en metros
-    '''
-    R = 6378000 + alt
-    
-    return MU/(R**2)
