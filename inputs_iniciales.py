f00926293f3b2caa61810d7b8ff122d4898f596f
diego17junio <37074539+diego17junio@users.noreply.github.com>
2018-03-22 18:48:30 +0100

    Add files via upload
    He creado un nuevo módulo con las inputs que habría que modificar para obtener diferentes resultados (altura, velocidad inicial, características geométricas del misil)

---
+++ inputs_iniciales.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+@author: TeamREOS
+Módulo que contiene los inputs del programa
+"""
+
+# Inputs de altura, velocidad y masa del avión.
+MASS = 14273  # Masa del avión cargado (kg).
+Z0 = 12000  # Altitud inicial del misil (m).
+M1 = 0.8  # Número de Mach inicial.
+
+# Caracterísiticas geométricas del misil.
+
+DIAMETRO_M = .5  # Diámetro del misil (m).
+LONGITUD_CONO = .9  # Longitud del cono del misil (m).
+LONGITUD_MISIL = 3  # Longitud total del misil (m).
+
+ESPESOR_ALETA = .0065  # Espesor de la aleta (m).
+CMEDIA_ALETA = .18  # Cuerda media de la aleta (m).
+CRAIZ_ALETA = .24  # Cuerda raiz de la aleta (m).
+
+SW_ALETA = .07875  # Superficie de una aleta del AIM (tomado como ref., m2).
+NUM_ALETAS = 4  # Número de aletas.
