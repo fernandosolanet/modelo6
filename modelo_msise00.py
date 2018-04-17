6eaa41b556e19e0dc585183e10729c36d11a3727
fernandosolanet <36764400+fernandosolanet@users.noreply.github.com>
2018-04-17 14:58:16 +0200

    Actualizo coeficientes
    Para obtener resultados precisos, es necesaria más precisión en los coeficientes de la densidad.

--- modelo_msise00.py
+++ modelo_msise00.py
@@ -1,17 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-
 @author: Team REOS
-
 Modelo atmosférico: NRL-MSISE00
-
 Funciones de temperatura (temperature), densidad (density), presión
 (pressure) y viscosidad (viscosity).  Sólo requieren una variable de
 entrada: la altitud.
-
 Los datos se obtienen de la página
 https://ccmc.gsfc.nasa.gov/modelweb/models/nrlmsise00.php
-
 """
 
 
@@ -32,7 +27,7 @@ TEMPER = [[288.4, -.002696, -1.209e-6, 1.105e-10, -2.703e-15],  # 0-11
           [1857, -.05847, 6.9745e-7, -2.8105e-12],  # 71-85
           [-4371, .1357, -1.114e-6, -2.772e-13, 2.41e-17],  # 85-105
           [17010, -.4213, 3.43e-6, -8.97e-12],  # 105-125
-          [-5509, .095195, -4.916e-7, 8.732e-12],  # 125-180
+          [-5498.54, .0950362, -4.90839e-7, 8.72167e-13],  # 125-180
           [-2638, .0488, -2.58e-7, 6.156e-13, -5.569e-19],  # 180-300
           [8.9638, .010298, -4.8383e-8, 1.1428e-13, -1.3543e-19, 6.4307e-26],
           # 300-440
@@ -41,26 +36,30 @@ TEMPER = [[288.4, -.002696, -1.209e-6, 1.105e-10, -2.703e-15],  # 0-11
 DENSIT = [[1.2113, -1.0767e-4, 2.8143e-9],  # 0-11
           [1.144, -9.3249e-5, 2.0283e-9],  # 11-20
           [.98121, -8.5625e-5, 2.59e-9, -2.6866e-14],  # 20-32
-          [.29811, -1.8891e-5, 4.0648e-10, -2.9534e-15],  # 32-47
+          [.53696369783, -4.3597202128e-5, 1.3582294965e-9, -1.9138461925e-14,
+           1.0253010619e-19],  # 32-47
           [.030063, -1.0345e-6, 9.0937e-12],  # 47-51
           [.028566, -1.1743e-6, 1.6299e-11, -7.6239e-17],  # 51-71
-          [.0083857, -2.9077e-7, 3.3802e-12, -1.3163e-17],  # 71-85
-          [.018477, -9.0294e-7, 1.7721e-11, -1.7448e-16, 8.6143e-22,
-           -1.7053e-27],  # 85-105
-          [5.0312e-4, -1.6648e-8, 2.0688e-13, -1.1439e-18, 2.3743e-24],
-          # 105-125
-          [1.4975e-5, -4.693e-10, 5.8873e-15, -3.696e-20, 1.578e-25,
-           -1.4508e-31],  # 125-180
-          [2.9019e-8, -4.3115e-13, 2.4264e-18, -6.1061e-24, 5.7825e-30],
-          # 180-300
-          [5.7268e-10, -4.077e-15, 9.7777e-21, -7.8765e-27],  # 300-440
-          [1.481e-10, -1.0922e-15, 3.2335e-21, -4.7949e-27, 3.5567e-33,
-           -1.0547e-39]]  # 440-800
+          [.021858322344, -9.8512966001e-7, 1.6779533018e-11,
+           -1.279121248e-16, 3.6795165237e-22],  # 71-85
+          [.0184773359, -9.02942889e-7, 1.77206449e-11, -1.74478699e-16,
+           8.61428221e-22, -1.70529948e-27],  # 85-105
+          [.0021797892824, -8.9868100230e-8, 1.4845673183e-12,
+           -1.2280410364e-17, 5.0858382431e-23, -8.4346471933e-29],  # 105-125
+          [6.21790234479e-5, -2.35674472104e-9, 3.72303267358e-14,
+           -3.13620341158e-19, 1.48529238085e-24, -3.74874972816e-30,
+           3.938525302088e-36],  # 125-180
+          [1.6244922565e-7, -3.6911722938e-12, 3.530199049e-17,
+           -1.8124348491e-22, 5.2555686765e-28, -8.1472260337e-34,
+           5.2685895499e-40],  # 180-300
+          [3.2983479421e-9, -3.9057260867e-14, 1.8766393702e-19,
+           -4.5571029771e-25, 5.5779327744e-31, -2.7476359591e-37],  # 300-440
+          [3.17056098e-10, -2.79591705e-15, 1.03325853e-20, -2.04426659e-26,
+           2.28016783e-32, -1.35785279e-38, 3.36960028e-45]]  # 440-800
 
 
 def interval_msise00(alt):
     '''División de tramos del modelo atmosférico MSISE00
-
     La variable de entrada alt es la altitud (m).  Debe ser menor o
     igual que 800000 (8e5) metros.
     '''
@@ -96,10 +95,8 @@ def interval_msise00(alt):
 def temperature(alt):
     '''Cálculo de la temperatura en función de la altura dada por el
     modelo MSISE00.
-
     La variable de entrada alt es la altitud (m).  Debe ser menor o
     igual que 800000(8e5) metros.
-
     La variable de salida es un float con la temperatura (K).
     '''
     tem = 0
@@ -112,10 +109,8 @@ def temperature(alt):
 def density(alt):
     '''Cálculo de la densidad en función de la altura dada por el modelo
     MSISE00.
-
     La variable de entrada alt es la altitud (m).  Debe ser menor o
     igual que 800000 (8e5) metros.
-
     La variable de salida es un float con la densidad (kg/m3).
     '''
     den = 0
@@ -128,10 +123,8 @@ def density(alt):
 def pressure(alt):
     '''Cálculo de la presión en función de la altura dada por el modelo
     MSISE00.  Se implementa la ley de los gases ideales.
-
     La variable de entrada alt es la altitud (m).  Debe ser menor o
     igual que 800000 (8e5) metros.
-
     La variable de salida es un float con la presión (Pa).
     '''
     return density(alt) * R_AIR * temperature(alt)
@@ -140,10 +133,8 @@ def pressure(alt):
 def viscosity(alt):
     '''Cálculo de la viscosidad en función de la altura dada por el
     modelo MSISE00.  Se implementa la ley de Sutherland.
-
     La variable de entrada alt es la altitud (m).  Debe ser menor o
     igual que 800000 (8e5) metros.
-
     La variable de salida es un float con la presión (Pa).
     '''
     tem = temperature(alt)
