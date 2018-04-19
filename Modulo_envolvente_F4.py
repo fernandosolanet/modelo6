cc570e4b6c7a007f00092d0e758ec57fdd429984
PedroMMB <36886310+PedroMMB@users.noreply.github.com>
2018-04-19 13:16:40 +0200

    Update Modulo_envolvente_F4.py
    Algunos cambios introducidos. Comentadas las líneas de importar el módulo inputs_iniciales para poder correr el programa sin él. Al final, introducir los espacios por tema de estilo. Introducido también la parte de Peso > P6 y P1 > Peso para extrapolar en esos casos.

--- Modulo_envolvente_F4.py
+++ Modulo_envolvente_F4.py
@@ -11,10 +11,10 @@ polinómica de alto orden para recrear las ecuaciones de los límites de la
 envolvente para el PESO de entrada.
 """
 
-from inputs_iniciales import MASS
+#from inputs_iniciales import MASS
 
 # Definción de variables. Todos los pesos en libras
-PESO = MASS # Para cuando quitemos la línea de abajo e importemos directamente del otro módulo
+#PESO = MASS # Para cuando quitemos la línea de abajo e importemos directamente del otro módulo
 PESO = 47300.0  # Input de entrada. Peso del avión cargado con el cohete REOS
 P1 = 42777.0 
 P2 = 43035.0
@@ -38,8 +38,18 @@ for i in F:  # Bucle para recorrer el archivo de texto
     # Para el último punto se realiza una interpolación lineal en el número
     # de Mach para igual valor final de altitud (36000ft)
     # Para PESO < P1 y para PESO > P6 lo que se realiza es una extrapolación 
+    
     if PESO < P1:
-        print("ERROR")
+        if i[0] < 0.9:
+            cte = -0.000478303
+        elif 0.9 <= i[0] >= 1:
+            cte = -0.000322528
+        elif 1 < i [0]:
+            cte = -0.001747212
+        
+        enve = i[1] + cte * (PESO - P1)
+        alt_enve.append(enve)
+        mach_enve.append(i[0])
     
     elif P1 < PESO < P2:
         enve = i[1] + (i[2]-i[1]) * (PESO - P1) / (P2 - P1)
@@ -79,19 +89,28 @@ for i in F:  # Bucle para recorrer el archivo de texto
             Mfin = i[11] + (i[12] - i[11]) * (PESO - P5) / (P6 - P5)
             
     elif PESO > P6:
-        print("ERROR")
+        if i[0] < 0.9:
+            cte = -0.00043214
+        elif 0.9 <= i[0] >= 1:
+            cte = -0.000693476
+        elif 1 < i [0]:
+            cte = -0.001720016
         
+        enve = i[1] + cte * (PESO - P6)
+        alt_enve.append(enve)
+        mach_enve.append(i[0])
+
 # Los siguientes condicionales añaden el último punto a las listas
-if P2<PESO<P3:
+if P2 < PESO < P3:
     mach_enve.append(Mfin)
     alt_enve.append(36.0)       
-elif P3<PESO<P4:
+elif P3 < PESO < P4:
     mach_enve.append(Mfin)
     alt_enve.append(36.0)
-elif P4<PESO<P5:
+elif P4 < PESO < P5:
     mach_enve.append(Mfin)
     alt_enve.append(36.0)
-elif P5<PESO<P6:
+elif P5 < PESO < P6:
     mach_enve.append(Mfin)
     alt_enve.append(36.0)
 
