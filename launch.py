956a138e0b58cb95d6d23fe437fd04210f03ab45
fernandosolanet <36764400+fernandosolanet@users.noreply.github.com>
2018-05-29 12:25:31 +0200

    Update launch.py

--- launch.py
+++ launch.py
@@ -3,6 +3,8 @@
 Created on Thu May  3 09:16:09 2018
 
 @author: fernando.solanet
+
+Este módulo sirve para
 """
 
 from time import time
@@ -17,13 +19,13 @@ from integracion import lanzamiento
 
 TIME = time()
 
-GASTOS = array([20, 20])  # Gasto másico (kg/s)
-ISPS = array([280, 280])  # Impulso específico (s)
+GASTOS = array([20, 20, 20])  # Gasto másico (kg/s)
+ISPS = array([280, 280, 280])  # Impulso específico (s)
 MASA_DE_PAGO = 40  # Masa de la carga de pago (kg)
-MASAS = array([635, 215, MASA_DE_PAGO])
+MASAS = array([635, 215, 110, MASA_DE_PAGO])
 MASA_TOTAL = float(sum(MASAS))
-ESTRUCTURAS = array([0.2, 0.2])  # Razón estructural
-RETARDOS = array([10, 5])
+ESTRUCTURAS = array([0.2, 0.2, 0.2])  # Razón estructural
+RETARDOS = array([10, 5, 5])
 # Tiempos de retardo de encendido de las etapas (s)
 DT = .05
 
@@ -54,16 +56,10 @@ if __name__ == '__main__':
                                          alt_maxima=7.999e5, perdidas=True,
                                          imprimir='14')
 
-#    print('\n\nÁngulo de asiento: '
-#          + format(90 - degrees(angulo_vectores(X, V)), '.2f') + ' deg')
     print('Velocidad del misil: '
           + format(norm(V) / vel_orbital(norm(X) - RT), '.3%')
           + ' de la velocidad orbital')
-#    print('Energía mecánica: ' + format(energia_mecanica(M, X, V), '.3e')
-#          + ' J')
 
-#    print('\nMasa final: ' + format(M, '.2f') + ' kg')
-#    print('\nTiempo de lanzamiento: ' + format(TIEMPO, '.3f') + ' s')
     print('Altura final: ' + format((norm(X) - RT) / 1000, '.3f') + ' km')
 
     print('\nMasa de la primera etapa: ' + format(MASAS[0], '.0f') + ' kg'
